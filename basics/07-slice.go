65bac680f31fc96d64cb7cfd1e3c1649305f543a
liujunwei <junwei.liu@easystack.cn>
2023-08-03 19:28:27 +0800

    Add basics example

--- basics/07-slice.go
+++ basics/07-slice.go
@@ -14,4 +14,15 @@ func main() {
 	s = append(s, "d")
 	s = append(s, "e", "f")
 	fmt.Println(s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println(c)
+	fmt.Println(s[2:5])
+	fmt.Println(s[:5])
+	fmt.Println(s[2:])
+
+	good := []string{"g", "o"}
+	fmt.Println(good)
+
 }
